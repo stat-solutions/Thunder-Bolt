7b2e01f783134bb7fce53ce3aa38c195f05e8b4d
GOOGOBAZ <augbazi@gmail.com>
2020-09-07 17:36:22 +0300

    First commit

---
+++ Client/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor(){
+
+
+  }
+
+
+  title = 'thunder-bolt-app';
+
+  updateME(){
+
+    this.title="Code 11 class";
+  }
+}
