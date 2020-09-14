3670e845aa25337e3b6ea0a3d73b427de3ba360d
Usama Idris <53287480+usamaidrsk@users.noreply.github.com>
2020-09-15 00:18:18 +0300

    structuring central management

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'authpage', redirectTo: '/authpage/loginpage', pathMatch: 'full' },
-  { path: '', redirectTo: '/authpage/loginpage', pathMatch: 'full' },
+  { path: 'authpage', redirectTo: '/authpage/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/authpage/login', pathMatch: 'full' },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'centralmanagement', loadChildren: () => import('./centralmanagement/centralmanagement.module').then(m => m.CentralmanagementModule) },
   { path: 'stationmanagement', loadChildren: () => import('./stationmanagement/stationmanagement.module').then(m => m.StationmanagementModule) },
--- Client/src/app/centralmanagement/components/areas/areas.component.ts
+++ Client/src/app/centralmanagement/components/areas/areas.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class AreasComponent implements OnInit {
 
   constructor() { }
-
+  areas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
   ngOnInit(): void {
+  }
+
+  createArea() {
+
   }
 
 }
